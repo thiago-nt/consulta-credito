1dd76116608c70d6a9b6dfa5e8f5d92ed83aa118
thiagoxpto <thiago.ntn@hotmail.com>
2025-04-09 00:00:08 -0300

    Adicionando back-end com contrução de estrutura inicial e documentação do swagger.

---
+++ consulta-credito-api/src/main/java/com/consulta/credito/dto/Credito.java
@@ -0,0 +1,77 @@
+package com.consulta.credito.dto;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Credito {
+
+  @NotNull
+  private String numeroCredito;
+
+  @NotNull
+  private String numeroNfse;
+
+  @NotNull
+  @Valid
+  private LocalDate dataConstituicao;
+
+  @NotNull
+  private Double valorIssqn;
+
+  @NotNull
+  private String tipoCredito;
+
+  @NotNull
+  private SimplesNacionalEnum simplesNacional;
+
+  @NotNull
+  private Double aliquota;
+
+  @NotNull
+  private Double valorFaturado;
+
+  @NotNull
+  private Double valorDeducao;
+
+  @NotNull
+  private Double baseCalculo;
+
+  public enum SimplesNacionalEnum {
+    SIM("Sim"),
+    NAO("Não");
+
+    private final String value;
+
+    SimplesNacionalEnum(String value) {
+      this.value = value;
+    }
+
+    @Override
+    @JsonValue
+    public String toString() {
+      return value;
+    }
+
+    @JsonCreator
+    public static SimplesNacionalEnum fromValue(String text) {
+      for (SimplesNacionalEnum b : SimplesNacionalEnum.values()) {
+        if (b.value.equalsIgnoreCase(text)) {
+          return b;
+        }
+      }
+      return null;
+    }
+  }
+}
